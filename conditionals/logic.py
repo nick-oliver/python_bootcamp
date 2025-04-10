46c0c28455b5a3947d6deb4248b984df3cab6c4c
nick-oliver <nick.oliver@secretary1419.local>
2025-04-10 15:21:50 -0500

    daily update

---
+++ conditionals/logic.py
@@ -0,0 +1,15 @@
+color = input("What's your favorite color ")
+if color == 'purple':
+	print("excellent choice!")
+elif color == 'teal':
+	print("not bad!")
+elif color == 'seafoam':
+	print("mediocre")
+elif color == 'pure darkness':
+	print("I like how you think")
+elif color == 'chartreuse':
+	print("How cute!")
+elif color == 'blue':
+	print("Are you a Master Mason?")
+else:
+	print("YOU MONSTER!!")
