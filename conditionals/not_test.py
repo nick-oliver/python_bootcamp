063ad6e9787c84dfde29bf5ade0bd602d2eb4f1e
nick-oliver <nick.oliver@secretary1419.local>
2025-04-17 08:38:41 -0500

    Linux VM update

---
+++ conditionals/not_test.py
@@ -0,0 +1,9 @@
+age = 7
+# 2-8 2 dollar ticket
+# 65 5 dollar ticket
+# 10 dollars for everyone else
+
+if not ((age >=2 and age <=8) or age >=65):
+	print("You are not a child or a senior so you pay full price")
+else:
+	print("you are a child or a senior")
---
+++ conditionals/or_test.py
@@ -0,0 +1,5 @@
+city = input("Where do you live? ")
+if city == "los angeles" or city == "san francisco":
+	print("You live in California")
+else:
+	print("You live in the real world")
